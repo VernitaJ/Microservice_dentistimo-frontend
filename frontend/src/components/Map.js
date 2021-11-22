8882812bc73871629a41161e9ed2b9fa5dfaf6cd
gsastraw <gussasgr@student.gu.se>
2021-11-22 17:23:38 +0100

    Finishing touches

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -5,17 +5,18 @@ import { GoogleMap, LoadScript } from '@react-google-maps/api';
 const API_KEY = process.env.REACT_APP_GOOGLEMAPS_APIKEY
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
-};
+  width: '100%',
+  height: '100vh',
+}
 
-// TODO: Use geolocation to center map towards user location
-const center = {
+// Gothenburg coordinates
+const defaultCenter = {
   lat: 57.6863144,
   lng: 11.9944233
 }
 
 const Map = () => {
+  // Permission to track location doesn't do anything currently. Just enabling location tracking for future implementations/updates.
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => console.log(position))
@@ -23,13 +24,14 @@ const Map = () => {
       alert('Geolocation is not supported by this browser.')
     }
   }) 
+
   return (
     <LoadScript
       googleMapsApiKey={API_KEY}
     >
       <GoogleMap
         mapContainerStyle={containerStyle}
-        center={center}
+        center={defaultCenter}
         zoom={10}
       >
         { /* Child components, such as markers, info windows, etc. */ }
