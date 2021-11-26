f92410c0ea858cfe33b2fe68c451c82914506fea
jonataa <jonataa@student.chalmers.se>
2021-11-26 11:10:45 +0100

    Added markers on map

--- src/App.js
+++ src/App.js
@@ -2,23 +2,6 @@ import './App.css';
 import Map from './components/Map'
 import React, { useEffect, useState } from 'react';
 import { Connector, useSubscription } from 'mqtt-react-hooks';
-import dentists from './resources/dentists.json'
-
-
-const Locations = () => {
-  let coordinates = []
-  const message =  dentists //useSubscription('frontend/dentists'); need to allign this with backend
-  message.dentists.forEach(e => { 
-  coordinates.push(e.coordinate)
-});
-  return (
-    <>
-      <span>{ JSON.stringify(coordinates) }</span>
-    </>
-  );
-};   
-
-
 
 function App() {
   return (
@@ -28,7 +11,6 @@ function App() {
           password: "1234"
       }
         }>
-      <Locations />
       <div>
          <Map 
          /> 
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-import { useSubscription } from 'mqtt-react-hooks';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
+import { useSubscription } from 'mqtt-react-hooks'
+import dentists from '../resources/dentists.json'
 
 // Create an .env in the frontend with a Maps JavaScript API key.
 const API_KEY = process.env.REACT_APP_GOOGLEMAPS_APIKEY
@@ -9,56 +10,40 @@ const containerStyle = {
   width: '100%',
   height: '100vh',
 }
-
+let coordinate
 // Gothenburg coordinates
 const defaultCenter = {
   lat: 57.6863144,
-  lng: 11.9944233
+  lng: 11.9944233,
 }
-
-/*
-
-## Test for mqtt connection add * <Children /> * component
-const Children = () => {
-  const { message, connectionStatus } = useSubscription('frontend/#');
-  const [messages, setMessages] = useState([]);
-
-  useEffect(() => {
-    if (message) setMessages((msgs) => [...msgs, message]);
-  }, [message]);
-
-  return (
-    <>
-      <span>{connectionStatus}</span>
-      <hr />
-      <span>{JSON.stringify(messages)}</span>
-    </>
-  );
-};   
-*/
-
-
-const Map  = (props) => {
-
+const Map = (props) => {
   // Permission to track location doesn't do anything currently. Just enabling location tracking for future implementations/updates.
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => console.log(position))
+      navigator.geolocation.getCurrentPosition((position) =>
+        console.log(position)
+      )
     } else {
       alert('Geolocation is not supported by this browser.')
     }
-  }) 
+  })
 
+  const message = dentists //useSubscription('frontend/dentists'); need to allign this with backend
   return (
-    <LoadScript
-      googleMapsApiKey={API_KEY}
-    >
+    <LoadScript googleMapsApiKey={API_KEY}>
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={defaultCenter}
         zoom={10}
       >
-        { /* Child components, such as markers, info windows, etc. */ }
+        {message.dentists.map((dentist) => {
+          return (<Marker
+            position={{
+              lat: dentist.coordinate.latitude,
+              lng: dentist.coordinate.longitude,
+            }}
+          />) //lat: dentist.coordinate.latitude, lng: dentist.coordinate.longitude
+        })}
         <></>
       </GoogleMap>
     </LoadScript>
