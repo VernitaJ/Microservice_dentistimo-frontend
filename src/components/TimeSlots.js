b9fa02d23dcca017225c5e654df5678fe5f09b84
jonataa <jonataa@student.chalmers.se>
2021-12-21 12:29:09 +0100

    error fix

--- src/components/TimeSlots.js
+++ src/components/TimeSlots.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { useMqttState, useSubscription } from 'mqtt-react-hooks'
+import { v4 as uuidv4 } from 'uuid'
 import {
   Button,
   Form,
@@ -12,10 +14,13 @@ import {
 } from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
 const TimeSlots = (props) => {
   let [collapse, setCollapse] = useState('-1')
   let [isOpen, setIsOpen] = useState(false)
+  const {client} = useMqttState()
   let name, email, mobile;
+
 
   const toggle = (index) => {
     setCollapse(index)
@@ -26,22 +31,32 @@ const TimeSlots = (props) => {
   if (props.timeslots) {
     dateslots= props.timeslots.response.filter((timeslot) => timeslot.startAt.substring(0,10) === props.date && timeslot.clinicId === props.clinicId)
   }
+  const handleSubmit = (startAt, endAt) => {
+    const request = {
+      requestId: uuidv4(),
+      clinicId: props.clinicId,
+      startAt: startAt,
+      endAt: endAt,
+  }
+    client.publish('frontend/booking/req', request)
+
+  }
 
   return (
     <div>
       {dateslots.length > 0 ? dateslots.map(timeSlot => (
-          <div key={timeSlot.startAt}>
+          <div key={timeSlot._id}>
             <Button
               color="primary"
-              onClick={() => toggle(timeSlot.id)}
+              onClick={() => toggle(timeSlot._id)}
               style={{ marginBottom: '1rem' }}
             >
               {timeSlot.startAt.substring(11,16)} - {timeSlot.endAt.substring(11,16)}
             </Button>
-            {timeSlot.id === collapse ? (
+            {timeSlot._id === collapse ? (
               <Modal isOpen={isOpen} className="booking-modal" backdrop={true}>
                 <ModalHeader>
-                  You've selected: {timeSlot.startTime} - {timeSlot.endTime}
+                  You've selected: {timeSlot.startAt.substring(11,16)} - {timeSlot.endAt.substring(11,16)}
                 </ModalHeader>
                 <ModalBody>
                   <Form>
@@ -81,7 +96,7 @@ const TimeSlots = (props) => {
                   </Form>
                 </ModalBody>
                 <ModalFooter>
-                  <Button color="primary" onClick={() => console.log('submit appointment for ', name, " for ", timeSlot.date, " email ", email)}>
+                  <Button color="primary" onClick={() => handleSubmit()}>
                     Confirm
                   </Button>{' '}
                   <Button color="secondary" onClick={() => setIsOpen(false)}>
