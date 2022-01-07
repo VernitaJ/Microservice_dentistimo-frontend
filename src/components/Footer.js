11b862badbeda97c848d0735a9fbcff862daa738
jonataa <jonataa@student.chalmers.se>
2022-01-07 14:06:49 +0100

    added link to repo docs and css <Vernite was here>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <footer className="inner-footer">
-        <div className="team">Team 9</div>
+        <a className="team" href="https://git.chalmers.se/courses/dit355/test-teams-formation/team-9/documentation">Team 9</a>
         <div className="year">©2021</div>
         <div className="email">
           <a className="email-link" href="mailto:dentistimomailer@gmail.com">
